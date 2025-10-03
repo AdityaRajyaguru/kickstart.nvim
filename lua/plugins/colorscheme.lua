90f47ea7677d3862abd2b0cbc592cf8b9541530b
Aditya Rajyaguru <47067778+AdityaRajyaguru@users.noreply.github.com>
2025-10-02 20:36:20 -0400

    Updated colorscheme to use onehalf

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -47,10 +47,29 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'onedark'
+      -- vim.cmd.colorscheme 'onedark'
 
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
+      vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = true })
+      vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = true })
+      vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F', bold = true })
+    end,
+  },
+  {
+    'MarcoKorinth/onehalf.nvim',
+    name = 'onehalf',
+    priority = 1000,
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      -- vim.cmd.colorscheme 'onedark'
+
+      -- You can configure highlights by doing something like:
+      -- vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'onehalfdark'
+
       vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = true })
       vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = true })
       vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F', bold = true })
